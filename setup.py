476e7ff5e094dfd90cd2736d9ae1d974020f8ba9
eurleif <eurleif@fa3f5ac0-6923-0410-a889-c47eab5a0760>
2007-01-08 23:03:19 +0000

    git-svn-id: http://svn.ecritters.biz/freecritters/trunk@3 fa3f5ac0-6923-0410-a889-c47eab5a0760

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="freeCritters"
+    name="freeCritters",
     version="0.1",
     description="Virtual pets.",
     author="Leif K-Brooks",
